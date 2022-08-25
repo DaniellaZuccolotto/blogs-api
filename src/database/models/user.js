d8c85e7247acc9df17358e0d57fff8b784eae38a
Daniella Zuccolotto <danielaz_9@hotmail.com>
2022-08-25 15:38:53 -0300

    feat: Criei a model de user

---
+++ src/database/models/user.js
@@ -0,0 +1,19 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: DataTypes.INTEGER,
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING, 
+  },
+  {
+    timestamps: false,
+  });
+
+  User.associate = (models) => {
+    User.hasMany(models.BlogPost,
+      { foreignKey: 'userId', as: 'blogposts' });
+  };
+
+  return User;
+};
