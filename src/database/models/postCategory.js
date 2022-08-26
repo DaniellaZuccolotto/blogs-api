6c324cfd638e8a34c6482d3f728f1af67757a65f
Daniella Zuccolotto <danielaz_9@hotmail.com>
2022-08-26 10:35:45 -0300

    feat: Criei o model PostCategory

---
+++ src/database/models/postCategory.js
@@ -0,0 +1,27 @@
+module.exports = (sequelize, DataTypes) => {
+  const PostCategory = sequelize.define(
+    'PostCategory',
+    {
+      postId: DataTypes.INTEGER,
+      categoryId: DataTypes.INTEGER,
+    },
+    { timestamps: false },
+  );
+
+  PostCategory.associate = (models) => {
+    models.BlogPost.belongsToMany(models.Category, {
+      as: 'category',
+      through: PostCategory,
+      foreignKey: 'postId',
+      otherKey: 'categoryId',
+    });
+    models.Category.belongsToMany(models.BlogPost, {
+      as: 'posts',
+      through: PostCategory,
+      foreignKey: 'categoryId',
+      otherKey: 'postId',
+    });
+  };
+
+  return PostCategory;
+};
